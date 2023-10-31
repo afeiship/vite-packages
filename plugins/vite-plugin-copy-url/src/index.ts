63f5f44ab8e917f6a32f96d1de7b8cfad4f5ba83
afeiship <1290657123@qq.com>
2023-10-31 14:05:23 +0800

    fix: default host

--- plugins/vite-plugin-copy-url/src/index.ts
+++ plugins/vite-plugin-copy-url/src/index.ts
@@ -5,7 +5,7 @@ const viteCopyURL = () => {
     name: 'vite-copy-url',
     configureServer({ config }) {
       const { host, port } = config.server;
-      const _host = host || '0.0.0.0';
+      const _host = host || 'localhost';
       const _port = port || 5173;
       const url = `http://${_host}:${_port}`;
       clipboardy.writeSync(url);
