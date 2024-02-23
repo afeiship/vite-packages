0602f3be184adec813038be3bb0eca77f3003608
afeiship <1290657123@qq.com>
2024-02-23 23:26:42 +0800

    fix: html banner plugin

--- plugins/vite-plugin-html-banner/src/index.ts
+++ plugins/vite-plugin-html-banner/src/index.ts
@@ -1,19 +1,18 @@
 import bannerDefaults from '@jswork/banner-defaults';
-import "@jswork/next";
-import "@jswork/next-nice-comments";
+import '@jswork/next';
+import '@jswork/next-nice-comments';
 
-type Options = { banner: string[]; }
+type Options = {
+  banner: string[];
+  pkg?: any;
+};
 
 export default (inOptions?: Options) => {
+  const { banner, pkg } = nx.mix({ banner: [], pkg: {} }, inOptions);
   return {
     name: 'vite-html-banner',
     transformIndexHtml(html) {
-      return (
-        nx.niceComments(
-          inOptions?.banner || bannerDefaults(),
-          'html'
-        ) + html
-      );
-    }
+      return nx.niceComments(banner || bannerDefaults(pkg), 'html') + html;
+    },
   };
 };
